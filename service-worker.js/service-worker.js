fb67c46f762b35300014ca4ad2e48b1e1d4603c0
ct24tatianaherrera <tatlsy625@gmail.com>
2025-04-23 14:57:52 -0500

    Initial commit

---
+++ service-worker.js/service-worker.js
@@ -0,0 +1,8 @@
+self.addEventListener('install', function(event) {
+    // Realiza la instalación
+  });
+  
+  self.addEventListener('fetch', function(event) {
+    // Maneja las solicitudes
+  });
+  
